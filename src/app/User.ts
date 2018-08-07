7ee75985cf5e7b4c6e0b83c622e8044e6fc0be5b
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2018-08-07 11:04:01 -0300

    Usando pipes

---
+++ src/app/User.ts
@@ -0,0 +1,6 @@
+export class User{
+    public name: string;
+    public age: number;
+
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from 'src/app/User';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  date: Date = new Date;
+  money: number = 598;
   title = 'Tasks';
+  user: User = {
+    name: 'Bob',
+    age: 25
+  }
+  upperText:string='Display';
+  lowerText:string='Display';
+  percentValue: number = 0.5;
 }
